7ed5257181b58ad44f6f938c9bcdf93139562e96
ling-ya <57176508+ling-ya@users.noreply.github.com>
2022-06-17 17:51:37 +0800

    删除不需要

--- DX_11/HLSL123/Cube_PS.hlsl
+++
@@ -1,7 +0,0 @@
-#include "Cube.hlsli"
-
-// ÏñËØ×ÅÉ«Æ÷
-float4 PS(VertexOut pIn) : SV_Target
-{
-    return float4(1,0,0,1);
-}
--- DX_11/HLSL123/Triangle_PS.hlsl
+++
@@ -1,8 +0,0 @@
-#include "Triangle.hlsli"
-
-// ÏñËØ×ÅÉ«Æ÷
-float4 PS(VertexOut pIn) : SV_Target
-{
-    return float4(0.0f,1.0f,0.0f,1.0f);
-    //return pIn.color;
-}
